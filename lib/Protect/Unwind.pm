8c007709a64bc7dde5b3fc0e1f197b504361b854
doug <doug@mercury.(none)>
2012-06-21 06:09:02 -0400

    docs

--- lib/Protect/Unwind.pm
+++ lib/Protect/Unwind.pm
@@ -53,18 +53,18 @@ Protect-Unwind - Safe cleanup blocks, Common Lisp style
 
 =head1 DESCRIPTION
 
-This module is just syntactic sugar around L<Guard> which implements an interface like Common Lisp's L<unwind-protect|http://www.lispworks.com/documentation/HyperSpec/Body/s_unwind.htm>.
+This module is just syntactic sugar around L<Guard>. It implements an interface like Common Lisp's L<unwind-protect|http://www.lispworks.com/documentation/HyperSpec/Body/s_unwind.htm>.
 
 It only exists so that hopefully lisp programmers new to perl will find this module before they find the buggy L<Unwind::Protect>.
 
-Unwind::Protect concentrates too hard on maintaining C<wantarray> and hiding created stack frames which are "problems" nobody cares about. The only important feature of lisp's C<unwind-protect> is that the cleanup-form is always run once the protected-form goes out of scope, something that Unwind::Protect doesn't implement correctly.
+Unwind::Protect concentrates too hard on maintaining C<wantarray> and hiding created stack frames which are "problems" I don't care about. The only purpose of lisp's C<unwind-protect> is that the cleanup-form is always run once the protected-form goes out of scope, something that Unwind::Protect doesn't implement correctly.
 
 Also, this module's test-suite can be considered an add-on to the L<Guard> tests.
 
 
 =head1 SEE ALSO
 
-L<Guard> is one of perl's correct C<unwind-protect> implementations which is why this module uses it.
+L<Guard> is a correct and efficient perl C<unwind-protect> implementation which is why this module uses it.
 
 
 =head1 AUTHOR
