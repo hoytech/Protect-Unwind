5d7c20d230d40199c3844759f070ce36a55392fc
doug <doug@mercury.(none)>
2012-10-13 01:55:14 -0400

    Releasing 0.300

--- lib/Protect/Unwind.pm
+++ lib/Protect/Unwind.pm
@@ -4,7 +4,7 @@ use strict;
 
 use Guard;
 
-our $VERSION = '0.1';
+our $VERSION = '0.300';
 
 use base 'Exporter';
 our @EXPORT = qw(protect unwind);
@@ -56,8 +56,6 @@ Protect-Unwind - Safe cleanup blocks, Common Lisp style
 This module is just syntactic sugar around L<Guard>. It implements an interface like Common Lisp's L<unwind-protect|http://www.lispworks.com/documentation/HyperSpec/Body/s_unwind.htm>.
 
 It only exists so that hopefully lisp programmers new to perl will find this module before they find the buggy L<Unwind::Protect>.
-
-Unwind::Protect concentrates too hard on maintaining C<wantarray> and hiding created stack frames which are "problems" I don't care about. The only purpose of lisp's C<unwind-protect> is that the cleanup-form is always run once the protected-form goes out of scope, something that Unwind::Protect doesn't implement correctly.
 
 Note that if your unwind forms throw exceptions the behaviour is somewhat complicated (see the L<Guard> docs).
 
